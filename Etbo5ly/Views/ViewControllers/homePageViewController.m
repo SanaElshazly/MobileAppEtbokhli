48f348fabd18f5777e9e2ea7f5fd0b77a7d49a34
MDW Event <mdwevent@jetss-mac-mini-2.local>
2016-05-22 16:43:05 +0200

    updating views

---
+++ Etbo5ly/Views/ViewControllers/homePageViewController.m
@@ -0,0 +1,33 @@
+//
+//  homePageViewController.m
+//  Etbo5ly
+//
+//  Created by ITI on 5/21/16.
+//  Copyright (c) 2016 JETS. All rights reserved.
+//
+
+#import "homePageViewController.h"
+#import "SWRevealViewController.h"
+@interface homePageViewController ()
+
+@end
+
+@implementation homePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    _viewSlideMenuButtonItem.target=self.revealViewController;
+    _viewSlideMenuButtonItem.action=@selector(revealToggle:);
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
