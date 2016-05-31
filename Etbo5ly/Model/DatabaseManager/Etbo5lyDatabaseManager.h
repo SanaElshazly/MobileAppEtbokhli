982da88c7ac55ce60153cccfc7afa071ef0cb43d
Sana Elshazly <sanaelshazly1993@gmail.com>
2016-05-31 13:41:07 +0200

    database manager created

---
+++ Etbo5ly/Model/DatabaseManager/Etbo5lyDatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  Etbo5lyDatabaseManager.h
+//  Etbo5ly
+//
+//  Created by Sana Elshazly on 5/31/16.
+//  Copyright © 2016 JETS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Etbo5lyDatabaseManager : NSObject
+
+@end
