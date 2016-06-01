3f92d2797d8390a883ad36b3ff60d0c066393cf6
MDW Event <mdwevent@jetss-mac-mini-2.local>
2016-06-01 19:20:08 +0200

    menu item view controller

---
+++ Etbo5ly/Views/ViewControllers/MealDetailedViewController.h
@@ -0,0 +1,13 @@
+//
+//  MealDetailedViewController.h
+//  Etbo5ly
+//
+//  Created by MDW Event on 6/1/16.
+//  Copyright © 2016 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MenuItems.h"
+@interface MealDetailedViewController : UIViewController
+@property (strong,nonatomic) MenuItems *detailedMenuItem;
+@end
---
+++ Etbo5ly/Views/ViewControllers/MealDetailedViewController.m
@@ -0,0 +1,37 @@
+//
+//  MealDetailedViewController.m
+//  Etbo5ly
+//
+//  Created by MDW Event on 6/1/16.
+//  Copyright © 2016 JETS. All rights reserved.
+//
+
+#import "MealDetailedViewController.h"
+
+@interface MealDetailedViewController ()
+
+@end
+
+@implementation MealDetailedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
